42ca2d8fc6caa2a6a6dc162b7e6c2b547bc83494
FEDDY1919 <72752593+FEDDY1919@users.noreply.github.com>
2022-08-30 20:30:31 +0800

    Update comment.js

--- models/comment.js
+++ models/comment.js
@@ -1,26 +1,23 @@
-const mongoose = require('mongoose')
-
-
+const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
-
   username: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: "User",
   },
-  content:{
-    type:String,
-    required:true
+  content: {
+    type: String,
+    required: true,
   },
   date: Date,
-})
+});
 
-commentSchema.set('toJSON', {
+commentSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  } //transforms id to string and removes _v value, idk if you need the _v value
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }, //transforms id to string and removes _v value, idk if you need the _v value
+});
 
-module.exports = mongoose.model('Comment', commentSchema)
+module.exports = mongoose.model("Comment", commentSchema);
