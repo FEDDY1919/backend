a081e06c75abc7ff0630f7ad18220dbf657d315d
FEDDY1919 <72752593+FEDDY1919@users.noreply.github.com>
2022-08-30 19:30:00 +0800

    '2nd'

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,20 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('products')
+  response.json(users)
+})
+
+usersRouter.get('/:id', async (request, response) => {
+  const user = await User.findById(request.params.id)
+  if (user) {
+    response.json(user)
+  } else {
+    response.status(404).end()
+  }
+})
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
@@ -25,9 +39,29 @@ usersRouter.post('/', async (request, response) => {
   response.status(201).json(savedUser)
 })
 
-usersRouter.get('/', async (request, response) => {
-    const users = await User.find({}).populate('products')
-    response.json(users)
+usersRouter.delete('/:id', async (request, response, next) => {
+  await User.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
+
+usersRouter.put('/:id', async(request, response, next) => {
+  const body = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+  const user = {
+    username:body.username,
+    name: body.name,
+    passwordHash:passwordHash
+  }
+
+  User.findByIdAndUpdate(request.params.id, user, { new: true })
+    .then(updatedProduct => {
+      response.json(updatedProduct)
+    })
+    .catch(error => next(error))
+})
+
 
 module.exports = usersRouter
